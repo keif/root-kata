5f7c29edf03cf69751366a256029254b63bce889
Rev. Keith T. Baker <keith_baker@abercrombie.com>
2019-01-22 12:34:13 -0500

    fix getDriversRecordsOutput test

--- src/utils/driverUtils.spec.js
+++ src/utils/driverUtils.spec.js
@@ -3,6 +3,7 @@ import Driver from "../Driver";
 import { getDriversRecordsOutput, sortDrivers, validateDriverRecords } from "./driverUtils";
 
 const driveTimes = [["07:15", "07:45"], ["06:12", "06:32"], ["12:01", "13:16"]];
+const driveDist = [17.3, 42.0, 0];
 const hardCodedDriversNotSorted = [];
 
 describe("driverUtils.js", () => {
@@ -11,22 +12,20 @@ describe("driverUtils.js", () => {
         hardCodedDriversNotSorted.push(new Driver("Alex"));
         hardCodedDriversNotSorted.push(new Driver("Bob"));
 
-        let i = 0;
-        hardCodedDriversNotSorted.forEach(driver => {
-            driver.setTotalTimeInHrs(driveTimes[i]);
-            driver.setTotalDistInMiles(i += 1);
+        hardCodedDriversNotSorted.forEach((driver, idx) => {
+            driver.setTotalTimeInHrs(driveTimes[idx]);
+            driver.setTotalDistInMiles(driveDist[idx]);
             driver.setMilesPerHour();
         });
     });
 
     describe("getDriversRecordsOutput method", () => {
         test("returns output of each drivers travel experiences", () => {
-            let expectedDriverOutput = "Dan: 0 miles\n";
-            expectedDriverOutput += "Alex: 1 miles @ 3 mph\n";
-            expectedDriverOutput += "Bob: 2 miles @ 2 mph\n";
-            const driverOutput = getDriversRecordsOutput(hardCodedDriversNotSorted);
+            let expectedDriverOutput = "Dan: 17.3 miles @ 35 mph\n";
+            expectedDriverOutput += "Alex: 42 miles @ 126 mph\n";
+            expectedDriverOutput += "Bob: 0 miles\n";
 
-            expect(driverOutput).toEqual(expectedDriverOutput);
+            expect(getDriversRecordsOutput(hardCodedDriversNotSorted)).toEqual(expectedDriverOutput);
         });
     });
 
