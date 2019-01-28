5b8a9b427da87269488b3bd5117b6622b7a1fa22
Rev. Keith T. Baker <keith_baker@abercrombie.com>
2019-01-28 01:03:58 -0500

    make readfile a little more generic.

--- src/readFile.js
+++ src/readFile.js
@@ -2,21 +2,11 @@ import process from "process";
 
 import { request } from "./request";
 
-import { createHash } from "./utils";
-import { getDriversRecordsOutput, sortDrivers, validateDriverRecords } from "./driverUtils";
-
-const readFile = () => {
-    console.log("readFile");
+const readFile = (callback) => {
     const file = process.argv[2];
-    console.log("file:", file);
 
     return request(file).then(data => {
-        console.log("file:", file);
-        const hashTbl = createHash(data);
-        const validDriversRecords = validateDriverRecords(hashTbl);
-        sortDrivers(validDriversRecords, "desc", "dist");
-
-        return getDriversRecordsOutput(validDriversRecords);
+        return callback(data);
     });
 };
 
--- src/readFile.spec.js
+++ src/readFile.spec.js
@@ -1,12 +1,14 @@
 // jest.mock("./request");
 
+import process from "process";
+
 import { readFile } from "./readFile";
 
-xdescribe("readFile.js", () => {
+describe("readFile.js", () => {
     describe("readFile method", () => {
         it("works with promises", () => {
-            console.log("tesT", readFile);
-            return readFile("data.text").then(data => expect(data).toEqual("Mark"));
+            process.argv = [, , "data.txt"];
+            return readFile().then(data => expect(data).toEqual("Mark"));
             expect.assertions(1);
         });
     });
